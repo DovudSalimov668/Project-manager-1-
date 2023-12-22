d7203164903667a10d1158c2f7eb0dcc74b4f242
Dovud Salimov <a9059262614@gmail.com>
2023-12-22 19:24:26 +0500

    first commit5

--- core/settings.py
+++ core/settings.py
@@ -66,7 +66,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMidlleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
